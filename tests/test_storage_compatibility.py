d2606cd288aa71b118440dbdba55568acd143be5
Muhammad Fithra Rizki <13523049@std.stei.itb.ac.id>
2026-08-22 15:19:54 +0700

    style: format storage compatibility tests

--- tests/test_storage_compatibility.py
+++ tests/test_storage_compatibility.py
@@ -49,9 +49,13 @@ class AppliedSchemaMigrationsCompatibilityTests(unittest.TestCase):
     def test_text_versions_are_preserved_exactly(self):
         repository, connection = self._repository(["001_init", "002_indexes"])
 
-        self.assertEqual(repository.applied_schema_migrations(), ["001_init", "002_indexes"])
-        self.assertEqual(connection.cursor_instance.executed,
-                         ["SELECT version FROM schema_migrations ORDER BY version"])
+        self.assertEqual(
+            repository.applied_schema_migrations(), ["001_init", "002_indexes"]
+        )
+        self.assertEqual(
+            connection.cursor_instance.executed,
+            ["SELECT version FROM schema_migrations ORDER BY version"],
+        )
         self.assertTrue(connection.entered)
         self.assertTrue(connection.exited)
         self.assertTrue(connection.cursor_instance.entered)
@@ -60,13 +64,17 @@ class AppliedSchemaMigrationsCompatibilityTests(unittest.TestCase):
     def test_utf8_bytes_are_decoded_in_query_order(self):
         repository, _ = self._repository([b"001_init", b"002_indexes"])
 
-        self.assertEqual(repository.applied_schema_migrations(), ["001_init", "002_indexes"])
+        self.assertEqual(
+            repository.applied_schema_migrations(), ["001_init", "002_indexes"]
+        )
 
     def test_mixed_text_and_bytes_are_returned_in_query_order(self):
         repository, _ = self._repository(["001_init", b"002_indexes", "003_final"])
 
-        self.assertEqual(repository.applied_schema_migrations(),
-                         ["001_init", "002_indexes", "003_final"])
+        self.assertEqual(
+            repository.applied_schema_migrations(),
+            ["001_init", "002_indexes", "003_final"],
+        )
 
     def test_empty_results_return_empty_list(self):
         repository, _ = self._repository([])
